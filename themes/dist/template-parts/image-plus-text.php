1db6c512d629397f7a39cd546f9367a21c67bd40
Michaela Hofer <michaela.hofer77@gmail.com>
2023-11-07 21:40:39 +0100

    paragraph styling options

--- themes/dist/template-parts/image-plus-text.php
+++ themes/dist/template-parts/image-plus-text.php
@@ -5,29 +5,67 @@ include 'loops/class.php';
 
 <?php
 $sectionTextImg = get_field('pic-plus-text');
+$headline = $sectionTextImg['headline_h2_'];
+
+$positionImg = '';
+if ($sectionTextImg['position'] == 'img-left') {
+    $positionImg = ' __img-left';
+}
+
+$headlineYEs = '';
+if($sectionTextImg['headline']) {
+    $headlineYEs = 'headline-h1';
+}
+
+$border = '';
+if ($headline['border']) {
+    $border = '__border';
+}
 
 $position = '';
-if ($sectionTextImg['position'] == 'img-left') {
-    $position = ' __img-left';
+if ($headline['text-direction'] == 'middle') {
+    $position = ' __middle';
+}
+$fontSize = '';
+if ($headline['font-size'] == 'small') {
+    $fontSize = ' __font-small';
+} elseif ($headline['font-size'] == 'medium') {
+    $fontSize = ' __font-medium';
+}
+$form = '';
+if ($sectionTextImg['form'] == 'round') {
+    $form = ' __round';
 }
 ?>
 
 <section>
+
     <div class="paragraphs">
-        <?php echo $sectionTextImg['h1']; ?>
-        <?php
-        $rows = $sectionTextImg['paragraphs'];;
-        if ($rows) {
-            echo '<ul>';
-            foreach ($rows as $row) {
-                echo '<li class="paragraph">';
-                echo $row['text'];
-                echo '</li>';
+        <div class="para-headline-container">
+            <h2 class="<?php echo $headlineYEs; echo $border;
+            echo $position;
+            echo $fontSize ?>">
+            <span class="headline-container">
+                <?php echo $headline['h1'] ?>
+                <span class="color1" style="border-color:<?php echo $headline['border_1']; ?>"></span>
+                <span class="color2" style="border-color:<?php echo $headline['border_2']; ?>"></span>
+            </span>
+            </h2>
+
+            <?php
+            $rows = $sectionTextImg['paragraphs'];;
+            if ($rows) {
+                echo '<ul>';
+                foreach ($rows as $row) {
+                    echo '<li class="paragraph">';
+                    echo $row['text'];
+                    echo '</li>';
+                }
+                echo '</ul>';
             }
-            echo '</ul>';
-        }
-        ?>
-        <div class="paragraph-img <?php echo $position ?>"><?php echo wp_get_attachment_image($sectionTextImg['img'], 'large'); ?></div>
+            ?>
+        </div>
+        <div class="paragraph-img <?php echo $positionImg; echo $form ?>"><?php echo wp_get_attachment_image($sectionTextImg['img'], 'large'); ?></div>
     </div>
 </section>
 
