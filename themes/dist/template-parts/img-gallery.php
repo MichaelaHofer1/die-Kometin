fc9a944985e92cd4d092d888a1b07f3c92f72232
Michaela Hofer <michaela.hofer77@gmail.com>
2024-02-13 21:40:29 +0100

    lightbox

--- themes/dist/template-parts/img-gallery.php
+++ themes/dist/template-parts/img-gallery.php
@@ -8,39 +8,40 @@ if (!empty($block['className'])) {
     $class_name .= ' ' . esc_attr($block['className']);
 }
 $gallery = get_field('img-gallery');
-$counter = 1;
+$counter = 0;
 $images = $gallery['imgs'];
 
 if ($images): ?>
-<section id="slideshow">
-    <div  id="gallery-cont">
-        <div class="gallery">
-            <ul>
-                <?php foreach ($images as $image_id): ?>
-                    <li class="gallery-imgs--visible <?php echo $counter; ?>">
-                        <?php echo wp_get_attachment_image($image_id, 'large'); ?>
-                    </li>
-                    <?php $counter++;
-                endforeach;?>
-            </ul>
+    <section id="slideshow" class="lightbox-hidden">
+        <div id="gallery-cont">
+            <div class="gallery">
+                <ul>
+                    <?php foreach ($images as $image_id): ?>
+                        <li class="gallery-imgs--visible" data-number="<?php echo $counter; ?>">
+                            <?php echo wp_get_attachment_image($image_id, 'large'); ?>
+                        </li>
+                        <?php $counter++;
+                    endforeach; ?>
+                </ul>
+            </div>
         </div>
-    </div>
 
 
-    <div class="splide slideshow-gallery-imgs--hidden <?php echo $class_name ?>" <?php echo $anchor; ?> role="group">
-        <div class="splide__track">
-            <ul class="splide__list slideshow">
-                <?php foreach ($images as $image_id): ?>
-                <li class="splide__slide slide--<?php echo $counter; ?>">
-                    <?php echo wp_get_attachment_image($image_id, 'large'); ?>
-                </li>
-                <?php $counter++;
-                endforeach;?>
-            </ul>
+        <div class="splide slideshow-gallery-imgs--hidden <?php echo $class_name ?>" <?php echo $anchor; ?>
+             role="group">
+            <div class="splide__track">
+                <ul class="splide__list slideshow">
+                    <?php foreach ($images as $image_id): ?>
+                        <li class="splide__slide slide--<?php echo $counter; ?>">
+                            <?php echo wp_get_attachment_image($image_id, 'large'); ?>
+                        </li>
+                        <?php $counter++;
+                    endforeach; ?>
+                </ul>
+            </div>
         </div>
-    </div>
+
+    </section>
     <div class="btn-close-lb"></div>
-
-</section>
 
 <?php endif; ?>
--- themes/src/assets/js/splide-slider-custommade.js
+++ themes/src/assets/js/splide-slider-custommade.js
@@ -2,15 +2,50 @@
 import Splide from '@splidejs/splide';
 
 document.addEventListener('DOMContentLoaded', function () {
+    const slideContainer = document.getElementById('slideshow')
+    const pictures = document.querySelectorAll('.gallery-imgs--visible')
+    const btnClose = document.querySelector('.btn-close-lb');
     const slideShowContainer = document.querySelector('.splide')
-    let numberOfSlide = 3
+    const bigLightbox = document.querySelectorAll('.splide__slide')
+    let numberOfSlide = 0
+
+    const openLightbox = function () {
+        if(slideContainer) {
+            pictures.forEach((slide) => {
+                slide.addEventListener('click', function () {
+                    numberOfSlide = slide.dataset.number;
+
+                    slideContainer.classList.remove('lightbox-hidden');
+                    slideContainer.classList.add('lightbox-active');
+
+                    splideSliderInitalize()
+                })
+            })
+
+
+            bigLightbox.forEach((box) => {
+                box.addEventListener('click', ( ) => {
+                    slideContainer.classList.add('lightbox-hidden');
+                    slideContainer.classList.remove('lightbox-active');
+
+                })
+            })
+
+
+            btnClose.addEventListener('click', () => {
+                slideContainer.classList.add('lightbox-hidden');
+                slideContainer.classList.remove('lightbox-active');
+            })
+        }
+    }
+
 
     const splideSliderInitalize = function () {
         let splide = new Splide('.splide', {
             start: numberOfSlide,
             type: 'loop',
             perPage: 1,
-            pagination: true,
+            pagination: false,
             mediaQuery: 'min',
             autoplay: false,
             easing: 'ease',
@@ -24,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     if (slideShowContainer) {
-        splideSliderInitalize()
+        openLightbox()
     }
 }, false)
 
